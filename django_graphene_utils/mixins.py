2f4bd8b133a3c4db43c039d94a1ecb757f4f41a8
Adrien Mille <adrien.mille.aer@gmail.com>
2017-08-25 15:34:29 +0200

    Fix critical error on ReduceMixin

--- django_graphene_utils/mixins.py
+++ django_graphene_utils/mixins.py
@@ -10,7 +10,7 @@ Mutation mixin to work with form applying the ReduceMixinForm
 class ReduceMixin(object):
     def get_form_kwargs(self, root, args, context, info):
         # ensure we can do it
-        assert issubclass(self._meta.form, ReduceMixinForm)
+        assert issubclass(self._meta.form_class, ReduceMixinForm)
         # get original keyword arguments
         kwargs = super(ReduceMixin, self).get_form_kwargs(root, args, context, info)
         # reduce the fields to the data we got in
