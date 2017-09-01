5b34e6c72ed6ec6ba538ac1dbb60bd7694368849
Adrien Mille <adrien.mille.aer@gmail.com>
2017-09-01 14:40:56 +0200

    Provide an implementation for basic django forms

--- django_graphene_utils/generic.py
+++ django_graphene_utils/generic.py
@@ -1,5 +1,4 @@
 import graphene
-from django import forms
 from django.db.models import QuerySet
 from django.utils import six
 from django.utils.decorators import classonlymethod
@@ -12,11 +11,11 @@ from .utils import convert_form_errors
 __all__ = ['ModelFormMutation', ]
 
 """
-Base class for model form mutation
+Base Form mutation
 """
 
 
-class BaseModelFormMutation(object):
+class BaseFormMutation(object):
     def __init__(self, **kwargs):
         self.form = None
 
@@ -28,27 +27,7 @@ class BaseModelFormMutation(object):
     def get_form_kwargs(self, root, args, context, info):
         return {
             'data': self.get_data(root, args, context, info),
-            'instance': self.get_instance(root, args, context, info)
         }
-
-    def get_instance(self, root, args, context, info):
-        if not self._meta.filter:
-            # we don't need to get an instance
-            return None
-
-        # get the queryset first
-        queryset = self._meta.queryset
-
-        # it might be a function to call
-        if callable(queryset):
-            # call it to get our queryset
-            queryset = queryset(root, args, context, info)
-
-        # ensure we've a queryset
-        assert isinstance(queryset, QuerySet)
-
-        # we may now get the object
-        return queryset.get(**dict(self._meta.filter(root, args, context, info)))
 
     def get_data(self, root, args, context, info):
         return args.get(self._meta.input_data_key, None)
@@ -76,16 +55,7 @@ class BaseModelFormMutation(object):
         return self.__class__._execute_chain(self, root, args, context, info)
 
     def get_successful_response(self, root, args, context, info, form):
-        response = {self._meta.output_success_key: True}
-
-        # save the form
-        instance = form.save(commit=self._meta.commit)
-
-        if self._meta.output_instance_key:
-            # we must provide the instance
-            response[self._meta.output_instance_key] = instance
-
-        return response
+        return {self._meta.output_success_key: True}
 
     def get_unsuccessful_response(self, root, args, context, info, form):
         # the error is obviously provide
@@ -124,7 +94,54 @@ class BaseModelFormMutation(object):
 
 
 """
-Options/settings for model form mutation
+Base class for model form mutation
+"""
+
+
+class BaseModelFormMutation(BaseFormMutation):
+    def get_form_kwargs(self, root, args, context, info):
+        # get original kwargs
+        kwargs = super(BaseModelFormMutation, self).get_form_kwargs(root, args, context, info)
+        # add the instance
+        kwargs['instance'] = self.get_instance(root, args, context, info)
+
+        return kwargs
+
+    def get_instance(self, root, args, context, info):
+        if not self._meta.filter:
+            # we don't need to get an instance
+            return None
+
+        # get the queryset first
+        queryset = self._meta.queryset
+
+        # it might be a function to call
+        if callable(queryset):
+            # call it to get our queryset
+            queryset = queryset(root, args, context, info)
+
+        # ensure we've a queryset
+        assert isinstance(queryset, QuerySet)
+
+        # we may now get the object
+        return queryset.get(**dict(self._meta.filter(root, args, context, info)))
+
+    def get_successful_response(self, root, args, context, info, form):
+        # get the original response
+        response = super(BaseModelFormMutation, self).get_successful_response(root, args, context, info)
+
+        # save the form
+        instance = form.save(commit=self._meta.commit)
+
+        if self._meta.output_instance_key:
+            # we must provide the instance
+            response[self._meta.output_instance_key] = instance
+
+        return response
+
+
+"""
+Options/settings for form mutation
 """
 
 
@@ -132,22 +149,35 @@ class Options(object):
     def __init__(self, options=None):
         # the model form class
         self.form_class = getattr(options, 'form', None)
-        assert issubclass(self.form_class, forms.ModelForm), \
-            '%r is an invalid form class' % self.form_class
 
         # the input keys
         self.input_data_key = getattr(options, 'input_data_key', 'data')
+
+        # the output keys
+        self.output_success_key = getattr(options, 'output_success_key', 'success')
+        self.output_error_key = getattr(options, 'output_error_key', 'errors')
+
+        # the registry
+        self.registry = getattr(options, 'registry', get_global_registry())
+
+        # middlewares
+        self.middlewares = getattr(options, 'middlewares', [])
+
+
+"""
+Options/settings for model form mutation
+"""
+
+
+class ModelOptions(Options):
+    def __init__(self, options=None):
+        super(ModelOptions, self).__init__(options)
 
         # should we commit
         self.commit = getattr(options, 'commit', True)
 
         # the output keys
-        self.output_success_key = getattr(options, 'output_success_key', 'success')
         self.output_instance_key = getattr(options, 'output_instance_key', None)
-        self.output_error_key = getattr(options, 'output_error_key', 'errors')
-
-        # the registry
-        self.registry = getattr(options, 'registry', get_global_registry())
 
         # we might have a queryset to follow
         self.queryset = getattr(options, 'queryset', None)
@@ -159,9 +189,6 @@ class Options(object):
         if self.queryset is None:
             # get the queryset from the model
             self.queryset = _get_queryset(self.model)
-
-        # middlewares
-        self.middlewares = getattr(options, 'middlewares', [])
 
 
 """
@@ -215,46 +242,37 @@ class ArgumentGetter:
 
 
 """
-Meta class model form mutation
+Meta class for form mutation
 """
 
 
-class ModelFormMutationMeta(type):
+class FormMutationMeta(type):
+    options_class = Options
+
     def __new__(mcs, name, bases, attrs):
         # build the new class
-        new_class = super(ModelFormMutationMeta, mcs).__new__(mcs, name, bases, attrs)
+        new_class = super(FormMutationMeta, mcs).__new__(mcs, name, bases, attrs)
 
-        if bases == (BaseModelFormMutation,):
+        if bases == (BaseFormMutation,):
             return new_class
 
         input_class = attrs.pop('Input', None)
         output_class = attrs.pop('Output', None)
 
         # get the meta class
-        opts = new_class._meta = Options(getattr(new_class, 'Meta', None))
-
-        if opts.filter is not None and not callable(opts.filter):
-            # handle it ourselves
-            opts.filter = ArgumentGetter(opts.filter)
+        opts = new_class._meta = mcs.options_class(getattr(new_class, 'Meta', None))
 
         # build the input class
         new_class._input = type('Input', (object,), props(input_class) if input_class else {})
 
         # build the output attributes
-        new_class._output_attrs = output_attrs = {
+        new_class._output_attrs = {
             # the common fields
             opts.output_success_key: graphene.Boolean(required=True),
             opts.output_error_key: graphene.List(FormError),
             # the custom ones
             **(props(output_class) if output_class else {}),
         }
-
-        if opts.output_instance_key is not None:
-            if not opts.output_instance_key in output_attrs:
-                # get the output type from the registry
-                output_type = opts.registry.get_type_for_model(opts.model)
-                # we have to handle it ourselves
-                output_attrs[opts.output_instance_key] = graphene.Field(output_type)
 
         # build the execute chain
         execute_chain = lambda self, root, args, context, info: self._execute(root, args, context, info)
@@ -265,6 +283,50 @@ class ModelFormMutationMeta(type):
         new_class._execute_chain = execute_chain
 
         return new_class
+
+
+"""
+Meta class for model form mutation
+"""
+
+
+class ModelFormMutationMeta(FormMutationMeta):
+    options_class = ModelOptions
+
+    def __new__(mcs, name, bases, attrs):
+        if bases == (BaseModelFormMutation,):
+            return super(FormMutationMeta, mcs).__new__(mcs, name, bases, attrs)
+
+        # build the new class
+        new_class = super(ModelFormMutationMeta, mcs).__new__(mcs, name, bases, attrs)
+
+        # get options
+        opts = new_class._meta
+
+        if opts.filter is not None and not callable(opts.filter):
+            # handle it ourselves
+            opts.filter = ArgumentGetter(opts.filter)
+
+        # get output attributes
+        output_attrs = new_class._output_attrs
+
+        if opts.output_instance_key is not None:
+            if not opts.output_instance_key in output_attrs:
+                # get the output type from the registry
+                output_type = opts.registry.get_type_for_model(opts.model)
+                # we have to handle it ourselves
+                output_attrs[opts.output_instance_key] = graphene.Field(output_type)
+
+        return new_class
+
+
+"""
+Usable class for form mutation
+"""
+
+
+class FormMutation(six.with_metaclass(FormMutationMeta, BaseFormMutation)):
+    pass
 
 
 """
